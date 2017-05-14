ed398a2824f7bac17764a6935a2e92e8acf52490
Robin Dtx <robin.deteix@polytechnique.edu>
2017-05-14 17:30:06 +0200

    Ajout fichiers de test

---
+++ Test/Test.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	std::cout << ("Hello World!!!");
+	return 0;
+}
