34faa616630ec496b6c6cfcd3eac7b6c7356f3fc
emmanuel <emmanuel.goutierre@mac.com>
2017-05-14 18:02:19 +0200

    Création de la classe RayDataStructure

---
+++ PROJET/RayDataStructure.h
@@ -0,0 +1,17 @@
+/*
+ * RayDataStructure.h
+ *
+ *  Created on: 14 mai 2017
+ *      Author: egoutierre
+ */
+
+#ifndef RAYDATASTRUCTURE_H_
+#define RAYDATASTRUCTURE_H_
+
+class RayDataStructure {
+public:
+	RayDataStructure();
+	virtual ~RayDataStructure();
+};
+
+#endif /* RAYDATASTRUCTURE_H_ */
