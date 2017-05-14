90d04c86ee92e828bf5bee25420270a8b15c73b1
emmanuel <emmanuel.goutierre@mac.com>
2017-05-14 18:35:22 +0200

    Destruction de la double création des setters

--- PROJET/RayDataStructure.h
+++ PROJET/RayDataStructure.h
@@ -7,20 +7,21 @@
 
 #ifndef RAYDATASTRUCTURE_H_
 #define RAYDATASTRUCTURE_H_
+#include "Vector.h"
 
 class RayDataStructure {
 	public:
 		RayDataStructure();
 		RayDataStructure(Vector origin, Vector direction);
 		virtual ~RayDataStructure();
-		setOrigin(Vector origin);
-		setDirection(Vector direction);
+		void setOrigin(Vector origin);
+		void setDirection(Vector direction);
 		Vector* getOrigin();
 		Vector* getDirection();
 
 	private:
-		Vector origin
-		Vector direction
+		Vector origin;
+		Vector direction;
 };
 
 #endif /* RAYDATASTRUCTURE_H_ */
