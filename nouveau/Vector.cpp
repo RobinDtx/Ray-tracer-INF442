e9e0aee4872a164992e50db0acc785a1d3a33a03
emmanuel <emmanuel.goutierre@mac.com>
2017-05-14 17:49:33 +0200

    test eclipse  Please enter the commit message for your changes. Lines starting

---
+++ nouveau/Vector.cpp
@@ -0,0 +1,18 @@
+/*
+ * Vector.cpp
+ *
+ *  Created on: 14 mai 2017
+ *      Author: egoutierre
+ */
+
+#include "Vector.h"
+
+Vector::Vector() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Vector::~Vector() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ nouveau/Vector.h
@@ -0,0 +1,17 @@
+/*
+ * Vector.h
+ *
+ *  Created on: 14 mai 2017
+ *      Author: egoutierre
+ */
+
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+class Vector {
+public:
+	Vector();
+	virtual ~Vector();
+};
+
+#endif /* VECTOR_H_ */
