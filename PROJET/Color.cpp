b52a69ea66b16945fe8c82bc3ffa95410f30aaa3
emmanuel <emmanuel.goutierre@mac.com>
2017-05-16 14:22:03 +0200

    Ajout d'une classe color

---
+++ PROJET/Color.cpp
@@ -0,0 +1,18 @@
+/*
+ * Color.cpp
+ *
+ *  Created on: 16 mai 2017
+ *      Author: egoutierre
+ */
+
+#include "Color.h"
+
+Color::Color() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Color::~Color() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ PROJET/Color.h
@@ -0,0 +1,17 @@
+/*
+ * Color.h
+ *
+ *  Created on: 16 mai 2017
+ *      Author: egoutierre
+ */
+
+#ifndef COLOR_H_
+#define COLOR_H_
+
+class Color {
+public:
+	Color();
+	virtual ~Color();
+};
+
+#endif /* COLOR_H_ */
