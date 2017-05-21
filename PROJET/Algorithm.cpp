76968b62f5789b393561fcec1a48f6a1da6de13a
emmanuel <emmanuel.goutierre@mac.com>
2017-05-21 20:28:51 +0200

    Debuguage Algorithm instersection

--- PROJET/Algorithm.cpp
+++ PROJET/Algorithm.cpp
@@ -6,7 +6,7 @@
  */
 
 #include "Algorithm.h"
-#include <Math.h>
+#include <math.h>
 
 Algorithm::Algorithm() {
 	// TODO Auto-generated constructor stub
@@ -32,6 +32,7 @@ static std::pair<bool, Vector*> ray_sphere_intersection(RayDataStructure* rd, Sp
 	double gamma = pow(o->x() - c->x(),2) + pow(o->y() - c->y(),2) + pow(o->z() - c->z(),2) -r*r;
 	double delta = beta*beta-4*alpha*gamma;
 
+
 	if(delta < 0){
 		return(std::make_pair(false, new Vector()));
 	}
@@ -43,19 +44,19 @@ static std::pair<bool, Vector*> ray_sphere_intersection(RayDataStructure* rd, Sp
 			return(std::make_pair(false, new Vector()));
 		}
 		else{
-			return(std::make_pair(true, Vector(o->x() + t2* d->x(), o->y() + t2*d->y(), o->z() + t2*d->z())));
+			return(std::make_pair(true, new Vector(o->x() + t2* d->x(), o->y() + t2*d->y(), o->z() + t2*d->z())));
 		}
 	}
 	else{
 		if(t2 < 0){
-			return(std::make_pair(true, Vector(o->x() + t1* d->x(), o->y() + t1*d->y(), o->z() + t1*d->z())));
+			return(std::make_pair(true, new Vector(o->x() + t1* d->x(), o->y() + t1*d->y(), o->z() + t1*d->z())));
 		}
 		else{
 			if(t1 < t2){
-				return(std::make_pair(true, Vector(o->x() + t1* d->x(), o->y() + t1*d->y(), o->z() + t1*d->z())));
+				return(std::make_pair(true, new Vector(o->x() + t1* d->x(), o->y() + t1*d->y(), o->z() + t1*d->z())));
 			}
 			else{
-				return(std::make_pair(true, Vector(o->x() + t2* d->x(), o->y() + t2*d->y(), o->z() + t2*d->z())));
+				return(std::make_pair(true, new Vector(o->x() + t2* d->x(), o->y() + t2*d->y(), o->z() + t2*d->z())));
 			}
 		}
 	}
