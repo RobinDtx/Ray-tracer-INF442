137e24ad2d04e6d71ff2b8e29d657321dfdab136
emmanuel <emmanuel.goutierre@mac.com>
2017-05-28 23:09:16 +0200

    changement de 2 ou 3 trucs pour refaire marcher mon eclipse'

--- PROJET/Debug/objects.mk
+++ PROJET/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lopencv_imgcodecs -lopencv_imgproc -lopencv_ml -lopencv_objdetect -lopencv_photo -lopencv_shape -lopencv_stitching -lopencv_superres -lopencv_video -lopencv_videoio -lopencv_videostab -lopencv_calib3d -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_highgui
+LIBS :=
 
